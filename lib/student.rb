7faea669f4f1835f495314856ef98e68d021a35e
David Little <littledavid175@gmail.com>
2019-08-23 03:28:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -11,15 +12,20 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |person|
+      Student.new(person)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+       # binding.pry
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
